8cebbd140bb21d3c8bb2789e6f6afa7fbb17ff73
G-Anubhav <anubhavgoyal868@gmail.com>
2025-05-23 17:32:07 +0530

    "New commit"

--- components/thank-you-page/NotifyForm.jsx
+++ components/thank-you-page/NotifyForm.jsx
@@ -1,6 +1,9 @@
 import { useForm } from "react-hook-form";
+import Image from "next/image";
+import ThankYouImage from "@/public/images/contact/thanks.png";
 import { toast, ToastContainer } from "react-toastify";
 import FadeInUp from "../animation/FadeInUp";
+import FadeInRight from "../animation/FadeInRight";
 import Field from "../common/Field";
 function NotifyForm() {
 	const {
@@ -17,6 +20,9 @@ function NotifyForm() {
 
 	return (
 		<FadeInUp>
+			{/* <FadeInRight className="sofax-thank-you-thumb center-image">
+              <Image src={ThankYouImage} alt="Thumbs" />
+            </FadeInRight> */}
 			<ToastContainer position="bottom-right" />
 			<div className="sofax-countdown-content sofax-section-title center max-width-large">
 				<h2>Thank you for Messaging Us!</h2>
@@ -28,7 +34,7 @@ function NotifyForm() {
 				<p>Feel Free to contact @ <a href="tel:+919354305099">9354305099</a></p>
 			</div>
 			<div className="extra-mt sofax-countdown-bottom-text">
-				<p><a href="/">Click Here</a> for Homepage</p>
+				<p>Go Back to HomePage <a href="/">Click Here</a></p>
 			</div>
 		</FadeInUp>
 	);
