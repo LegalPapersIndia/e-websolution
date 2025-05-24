679e1ae906eb4d87737a0c9300931619c29b32a7
G-Anubhav <anubhavgoyal868@gmail.com>
2025-05-24 12:26:11 +0530

    New Commit

--- components/common/topbar/index.jsx
+++ components/common/topbar/index.jsx
@@ -29,10 +29,10 @@ const TopBar = () => {
           </Link>
         </div>
         <div className="topbar-right">
-          <Link href="#"><FaFacebookF size={14} /></Link>
-          <Link href="#"><FaTwitter size={14} /></Link>
+          <Link href="https://www.facebook.com/ewebsolution.in/" target="_blank"><FaFacebookF size={14} /></Link>
+          <Link href="https://www.instagram.com/ewebsolution.in/" target="_blank"><FaInstagram size={15} /></Link>
           <Link href="#"><FaLinkedinIn size={14} /></Link>
-          <Link href="#"><FaInstagram size={15} /></Link>
+          <Link href="#"><FaTwitter size={14} /></Link>
           <Link href="#"><FaYoutube size={15} /></Link>
         </div>
       </div>
